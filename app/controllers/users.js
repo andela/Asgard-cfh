9db4f78abf1bf4e5ee4a683ce54be53cad7b4693
clintfidel <clintfidel@gmail.com>
2018-06-03 01:59:55 +0100

    [feature #157801309] Add image upload to signup form

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,11 +1,11 @@
 /**
  * Module dependencies.
  */
-let mongoose = require('mongoose'),
+const mongoose = require('mongoose'),
   User = mongoose.model('User');
-let avatars = require('./avatars').all();
+const avatars = require('./avatars').all();
 const jwt = require('jsonwebtoken');
-let bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 const secret = process.env.SECRET;
@@ -13,14 +13,14 @@ const secret = process.env.SECRET;
 /**
  * Auth callback
  */
-exports.authCallback = function (req, res, next) {
+exports.authCallback = (req, res) => {
   res.redirect('/chooseavatars');
 };
 
 /**
  * Show login form
  */
-exports.signin = function (req, res) {
+exports.signin = (req, res) => {
   if (!req.user) {
     res.redirect('/#!/signin?error=invalid');
   } else {
@@ -31,7 +31,7 @@ exports.signin = function (req, res) {
 /**
  * Show sign up form
  */
-exports.signup = function (req, res) {
+exports.signup = (req, res) => {
   if (!req.user) {
     res.redirect('/#!/signup');
   } else {
@@ -42,7 +42,7 @@ exports.signup = function (req, res) {
 /**
  * Logout
  */
-exports.signout = function (req, res) {
+exports.signout = (req, res) => {
   req.logout();
   res.redirect('/');
 };
@@ -59,18 +59,18 @@ exports.session = function (req, res) {
  * already has an avatar. If they don't have one, redirect them
  * to our Choose an Avatar page.
  */
-exports.checkAvatar = function (req, res) {
+exports.checkAvatar = (req, res) => {
   if (req.user && req.user._id) {
     User.findOne({
       _id: req.user._id
     })
       .exec((err, user) => {
-      if (user.avatar !== undefined) {
-        res.redirect('/#!/');
-      } else {
-        res.redirect('/#!/choose-avatar');
-      }
-    });
+        if (user.avatar !== undefined) {
+          res.redirect('/#!/');
+        } else {
+          res.redirect('/#!/choose-avatar');
+        }
+      });
   } else {
     // If user doesn't even exist, redirect to /
     res.redirect('/');
@@ -81,24 +81,24 @@ exports.checkAvatar = function (req, res) {
 /**
  * Create user
  */
-exports.create = function (req, res) {
+exports.create = (req, res, next) => {
   if (req.body.name && req.body.password && req.body.email) {
     User.findOne({
       email: req.body.email
-    }).exec((err,existingUser) => {
+    }).exec((err, existingUser) => {
       if (!existingUser) {
-        var user = new User(req.body);
+        const user = new User(req.body);
         // Switch the user's avatar index to an actual avatar url
         user.avatar = avatars[user.avatar];
         user.provider = 'local';
-        user.save(function(err) {
+        user.save((err) => {
           if (err) {
             return res.render('/#!/signup?error=unknown', {
               errors: err.errors,
-              user: user
+              user
             });
           }
-          req.logIn(user, function(err) {
+          req.logIn(user, (err) => {
             if (err) return next(err);
             return res.redirect('/#!/');
           });
@@ -112,7 +112,7 @@ exports.create = function (req, res) {
   }
 };
 
-exports.signUp = function (req, res) {
+exports.signUp = (req, res) => {
   const {
     name,
     password,
@@ -138,7 +138,8 @@ exports.signUp = function (req, res) {
             const { _id, email } = newUser;
             const token = jwt.sign({
               exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
-               _id, email 
+              _id,
+              email
             }, secret);
             return res.status(201).send({
               message: 'Signed up successfully',
@@ -154,7 +155,7 @@ exports.signUp = function (req, res) {
   }
 };
 
-exports.login = function (req, res) {
+exports.login = (req, res) => {
   if (!req.body.email || !req.body.password) {
     return res.status(406).json({
       error: 'plaese fill in required fields'
@@ -163,7 +164,7 @@ exports.login = function (req, res) {
   return User.findOne({
     email: req.body.email
   }).exec((err, user) => {
-    if(!user) {
+    if (!user) {
       return res.status(401).json({
         error: 'invalid credentials'
       });
@@ -184,17 +185,13 @@ exports.login = function (req, res) {
       });
     }
   });
-    // .then((user) => {
-      
-    // });
-  
 };
 
 
 /**
  * Assign avatar to user
  */
-exports.avatars = function (req, res) {
+exports.avatars = (req, res) => {
   // Update the current user's profile to include the avatar choice they've made
   if (req.user && req.user._id && req.body.avatar !== undefined &&
     /\d/.test(req.body.avatar) && avatars[req.body.avatar]) {
@@ -202,14 +199,14 @@ exports.avatars = function (req, res) {
       _id: req.user._id
     })
       .exec((err, user) => {
-      user.avatar = avatars[req.body.avatar];
-      user.save();
-    });
+        user.avatar = avatars[req.body.avatar];
+        user.save();
+      });
   }
   return res.redirect('/#!/app');
 };
 
-exports.addDonation = function (req, res) {
+exports.addDonation = (req, res) => {
   if (req.body && req.user && req.user._id) {
     // Verify that the object contains crowdrise data
     if (req.body.amount && req.body.crowdrise_donation_id && req.body.donor_name) {
@@ -218,19 +215,19 @@ exports.addDonation = function (req, res) {
       })
         .exec((err, user) => {
         // Confirm that this object hasn't already been entered
-        var duplicate = false;
-        for (var i = 0; i < user.donations.length; i++ ) {
-          if (user.donations[i].crowdrise_donation_id === req.body.crowdrise_donation_id) {
-            duplicate = true;
+          let duplicate = false;
+          for (let i = 0; i < user.donations.length; i += 1) {
+            if (user.donations[i].crowdrise_donation_id === req.body.crowdrise_donation_id) {
+              duplicate = true;
+            }
           }
-        }
-        if (!duplicate) {
-          console.log('Validated donation');
-          user.donations.push(req.body);
-          user.premium = 1;
-          user.save();
-        }
-      });
+          if (!duplicate) {
+            // console.log('Validated donation');
+            user.donations.push(req.body);
+            user.premium = 1;
+            user.save();
+          }
+        });
     }
   }
   res.send();
@@ -239,8 +236,8 @@ exports.addDonation = function (req, res) {
 /**
  *  Show profile
  */
-exports.show = function (req, res) {
-  let user = req.profile;
+exports.show = (req, res) => {
+  const user = req.profile;
 
   res.render('users/show', {
     title: user.name,
@@ -251,21 +248,21 @@ exports.show = function (req, res) {
 /**
  * Send User
  */
-exports.me = function (req, res) {
+exports.me = (req, res) => {
   res.jsonp(req.user || null);
 };
 
 /**
  * Find user by id
  */
-exports.user = function (req, res, next, id) {
+exports.user = (req, res, next, id) => {
   User
     .findOne({
       _id: id
     })
     .exec((err, user) => {
       if (err) return next(err);
-      if (!user) return next(new Error('Failed to load User ' + id));
+      if (!user) return next(new Error(`Failed to load User ${id}`));
       req.profile = user;
       next();
     });
