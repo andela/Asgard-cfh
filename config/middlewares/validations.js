d3eace54ce32e5c66560481cf502afc713ca2348
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-45.local>
2018-06-08 11:52:46 +0100

    add validator middleware

---
+++ config/middlewares/validations.js
@@ -0,0 +1,26 @@
+const { check, validationResult } = require('express-validator/check');
+/**
+ * @description - Validate user signup
+ * @param {object} req - request body
+ * @param {object} res - response body
+ *
+ * @return {undefined} depends on user input
+ */
+exports.validateSignup = (req, res, next) => {
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+  const errors = validationResult(req);
+  console.log(errors);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  return next();
+};
+
+ exports.signupChecks = [check('email', 'please enter a valid email')
+        .isEmail(),
+        check('name', 'name must be a minimum of two letters')
+        .trim()
+        .isLength({ min: 2 }),
+        check('password', 'passwords must be at least 6 chars long and contain one number')
+        .isLength({ min: 6 })
+        .matches(/\d/)];
