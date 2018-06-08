91d267e01dac148ad9cec587926295e666e629d1
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-45.local>
2018-06-08 11:54:56 +0100

    display user errors on fronted

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -12,16 +12,17 @@ angular.module('mean.system').controller('IndexController', [
   ($scope, Global, $http, $window, $location, $q, socket, game, AvatarService) => {
     $scope.global = Global;
 
-    
-
    $scope.playAsGuest = () => {
       game.joinGame();
       $location.path('/app');
     };
 
-    $scope.showError = false;
-
+    $scope.showLoginError = false;
+    $scope.hasSignupError = false;
+    $scope.SignupError = null;
+    $scope.dontShow = false;
     $scope.avatars = [];
+
     AvatarService.getAvatars()
       .then(function(data) {
         $scope.avatars = data;
@@ -60,6 +61,11 @@ angular.module('mean.system').controller('IndexController', [
             localStorage.setItem('token', response.data.token);
          //  $http.default.headers.common['x-access-token'] = response.data.token;
           $location.path('/');
+        }, (error) => {
+          if(error.data.errors) {
+            $scope.hasSignupError = true;
+            $scope.signupError = error.data.errors[0].msg
+          }
         })
           },
         })
@@ -69,6 +75,11 @@ angular.module('mean.system').controller('IndexController', [
           localStorage.setItem('token', response.data.token);
          //  $http.default.headers.common['x-access-token'] = response.data.token;
           $location.path('/');
+        }, (error) => {
+          if(error.data.errors) {
+            $scope.hasSignupError = true;
+            $scope.signupError = error.data.errors[0].msg
+          }
         })
       }
       }
@@ -79,7 +90,7 @@ angular.module('mean.system').controller('IndexController', [
         localStorage.setItem('token', response.data.token);
         $location.path('/');
       }, (error) => {
-        $scope.showError = true;
+        $scope.showSignupError = true;
       });
       
     }
