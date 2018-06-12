03fbac92a81aa6c7ec546629eed29ba5a0e2f96b
Fortune Ekeruo <legalmody@gmail.com>
2018-06-12 13:09:53 +0100

    Example it blocks

--- test/endpoints/authentication.js
+++ test/endpoints/authentication.js
@@ -5,12 +5,17 @@ import app from '../../server';
 
 const User = mongoose.model('User');
 
+// Request handler for making calls
+const request = supertest.agent(app);
+
 describe('Authentication', () => {
   describe('Signup', () => {
-
+    it('Should return a JWT upon user signup', () => {
+      should(1).equal(1);
+    });
   });
 
   describe('Signin', () => {
-
+    it('Should be able to login', () => should(2).equal(2));
   });
 });
