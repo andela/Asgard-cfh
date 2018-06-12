0cc06cc32a7fb4d6b1c9614fb4f81e2bb6fbc800
Fortune Ekeruo <legalmody@gmail.com>
2018-06-12 12:55:55 +0100

    Setup test for authenication endpoints

---
+++ test/endpoints/authentication.js
@@ -0,0 +1,16 @@
+import should from 'should';
+import mongoose from 'mongoose';
+import supertest from 'supertest';
+import app from '../../server';
+
+const User = mongoose.model('User');
+
+describe('Authentication', () => {
+  describe('Signup', () => {
+
+  });
+
+  describe('Signin', () => {
+
+  });
+});
