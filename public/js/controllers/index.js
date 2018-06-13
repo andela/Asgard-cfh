cf01fa0fc66826ffa5eb7dcd97ada8cfca4000c1
Saheed Ajibulu <stunnafrsh007@gmail.com>
2018-06-13 21:50:49 +0100

    [Feature 157801294] Resolve merge conflicts in public/js/controllers/index.js

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -10,70 +10,7 @@ angular.module('mean.system').controller('IndexController', [
   'game',
   'AvatarService',
   ($scope, Global, $http, $window, $location, $q, socket, game, AvatarService) => {
-    $scope.global = Global;
-
-<<<<<<< HEAD
-    /**self add */
-    $scope.myInterval = 5000;
-    // $scope.noWrapSlides = false;
-    // $scope.active = 0;
-    // const slides = $scope.slides = [];
-    // let currIndex = 0;
-
-    // $scope.addSlide = function() {
-    //   var newWidth = 600 + slides.length + 1;
-    //   slides.push({
-    //     image: '//unsplash.it/' + newWidth + '/300',
-    //     text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
-    //     id: currIndex++
-    //   });
-    // };
-  
-    // $scope.randomize = function() {
-    //   var indexes = generateIndexesArray();
-    //   assignNewIndexesToSlides(indexes);
-    // };
-  
-    // for (var i = 0; i < 4; i++) {
-    //   $scope.addSlide();
-    // }
-  
-    // // Randomize logic below
-  
-    // function assignNewIndexesToSlides(indexes) {
-    //   for (var i = 0, l = slides.length; i < l; i++) {
-    //     slides[i].id = indexes.pop();
-    //   }
-    // }
-  
-    // function generateIndexesArray() {
-    //   var indexes = [];
-    //   for (var i = 0; i < currIndex; ++i) {
-    //     indexes[i] = i;
-    //   }
-    //   return shuffle(indexes);
-    // }
-  
-    // // http://stackoverflow.com/questions/962802#962890
-    // function shuffle(array) {
-    //   var tmp, current, top = array.length;
-  
-    //   if (top) {
-    //     while (--top) {
-    //       current = Math.floor(Math.random() * (top + 1));
-    //       tmp = array[current];
-    //       array[current] = array[top];
-    //       array[top] = tmp;
-    //     }
-    //   }
-  
-    //   return array;
-    // }
-
-    /**self add end */
-
-=======
->>>>>>> develop
+   $scope.global = Global;
    $scope.playAsGuest = () => {
       game.joinGame();
       $location.path('/app');
