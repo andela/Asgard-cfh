759c3c4760380a7c9ee116e2f04edd3616b98da7
rotimi <isaiah.yemitan@gmail.com>
2018-06-13 10:55:09 +0100

    Add test for searching for users

--- test/endpoints/invite.spec.js
+++ test/endpoints/invite.spec.js
@@ -13,8 +13,22 @@ describe('POST to /invite', () => {
         token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjk1Mjg5MzIxMzEsIl9pZCI6IjViMjA1NWEzNDc1NGFhMDVmYjBlZjc5OSIsImVtYWlsIjoiZG91Z2xhc2VnaWVtZWhAZ21haWwuY29tIiwiaWF0IjoxNTI4ODQ1NzMxfQ.gOaq1u2hgcz4Hv_HJ8p0knOPIuXe_9PkoRqW0oJXx8g'
       })
       .end((err, res) => {
-        console.log(res);
         res.body.message.should.equal('Email sent successfully');
+        res.status.should.equal(200);
+        done();
+      });
+  });
+});
+
+describe('get to /search', () => {
+  it('Should search users', (done) => {
+    request
+      .post('/api/search')
+      .send({
+        term: '',
+      })
+      .end((err, res) => {
+        res.body.message.should.equal('Users Found');
         res.status.should.equal(200);
         done();
       });
