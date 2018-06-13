5b00e1f4de0865ecf84bfae7b2dc69c3ce116371
Douglas Egiemeh <douglasegiemeh@Andelas-MacBook-Pro-66.local>
2018-06-13 11:08:45 +0100

    clean up unwanted and commented out code

--- test/endpoints/auth.spec.js
+++ test/endpoints/auth.spec.js
@@ -2,16 +2,13 @@ import should from 'should';
 import mongoose from 'mongoose';
 import supertest from 'supertest';
 import app from '../../server';
-import Token from '../../app/controllers/users';
 
-const jwt = require('jsonwebtoken');
 
 const User = mongoose.model('User');
-const token = 'token';
 
 // Request handler for making API calls
 const request = supertest.agent(app);
- 
+
 describe('Authentication', () => {
   describe('Signup', () => {
     it('Should return a JWT upon user signup', () => {
